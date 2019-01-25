17ebce2325fe4e6199a2df7ba114f172164d9f27
Maxim_Chshelokov <schelokov.mv@gmail.com>
2019-01-25 15:58:54 +0600

    Reorganized unit tests

--- src/test/com/epam/dao/NewsDaoImplTest.java
+++ src/test/com/epam/dao/NewsDaoImplTest.java
@@ -1,6 +1,6 @@
 package com.epam.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
