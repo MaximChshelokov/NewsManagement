45fc44afba087469f90b42210e0311c8fcbd2f34
Maxim_Chshelokov <schelokov.mv@gmail.com>
2019-01-25 12:09:55 +0600

    Added dao unit test

---
+++ src/main/java/com/epam/dao/NewsDaoImplTest.java
@@ -0,0 +1,104 @@
+package com.epam.dao;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+import com.epam.model.News;
+import org.hibernate.IdentifierLoadAccess;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class NewsDaoImplTest {
+
+    @Mock
+    private SessionFactory sessionFactory;
+    @Mock
+    private Session session;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private News news = new News();
+    private static final long TEST_ID = 1L;
+    private NewsDaoImpl dao = new NewsDaoImpl();
+
+    @Before
+    public void setUp() throws Exception {
+        dao.setSessionFactory(sessionFactory);
+        when(sessionFactory.getCurrentSession()).thenReturn(session);
+    }
+
+    @Test
+    public void save() {
+        when(session.save(news)).then(n -> {
+            ((News) n.getArgument(0)).setId(TEST_ID);
+            return null;});
+        dao.save(news);
+        verify(session, times(1)).save(news);
+        assertEquals(news.getId(), TEST_ID);
+    }
+
+    @Test
+    public void get() {
+        when(session.get(News.class, TEST_ID)).thenReturn(news);
+        News result = dao.get(TEST_ID);
+        verify(session, times(1)).get(News.class, TEST_ID);
+        assertEquals(result, news);
+    }
+
+    @Test
+    public void getAll() {
+        CriteriaBuilder cb = mock(CriteriaBuilder.class);
+        CriteriaQuery<News> cq = mock(CriteriaQuery.class);
+        Root<News> root = mock(Root.class);
+        Query<News> query = mock(Query.class);
+        when(session.getCriteriaBuilder()).thenReturn(cb);
+        when(cb.createQuery(News.class)).thenReturn(cq);
+        when(cq.from(News.class)).thenReturn(root);
+        when(session.createQuery(cq)).thenReturn(query);
+        when(query.getResultList()).thenReturn(Collections.singletonList(news));
+
+        List<News> result = dao.getAll();
+        assertEquals(result.get(0), news);
+    }
+
+    @Test
+    public void update() {
+        setILA();
+        News newsToUpdate = new News();
+        newsToUpdate.setTitle("News");
+
+        dao.update(TEST_ID, newsToUpdate);
+        verify(session, times(1)).flush();
+        assertEquals(news.getTitle(), newsToUpdate.getTitle());
+    }
+
+    @Test
+    public void delete() {
+        setILA();
+        dao.delete(TEST_ID);
+        verify(session).delete(news);
+    }
+
+    private void setILA() {
+        IdentifierLoadAccess<News> ila = mock(IdentifierLoadAccess.class);
+        when(session.byId(News.class)).thenReturn(ila);
+        when(ila.load(TEST_ID)).thenReturn(news);
+    }
+}
--- src/main/java/com/epam/service/NewsServiceImplTest.java
+++ src/main/java/com/epam/service/NewsServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 import com.epam.dao.NewsDao;
 import com.epam.model.News;
@@ -40,14 +41,14 @@ public class NewsServiceImplTest {
     public void addMustReturnTrue() {
         when(dao.save(news)).thenReturn(1L);
         boolean result = service.add(news);
-        verify(dao).save(news);
+        verify(dao, times(1)).save(news);
         assertTrue(result);
     }
 
     @Test
     public void addMustReturnFalse() {
         boolean result = service.add(news);
-        verify(dao).save(news);
+        verify(dao, times(1)).save(news);
         assertFalse(result);
     }
 
@@ -56,7 +57,7 @@ public class NewsServiceImplTest {
         List<News> newsList = Collections.singletonList(news);
         when(dao.getAll()).thenReturn(newsList);
         List<News> result = service.getAll();
-        verify(dao).getAll();
+        verify(dao, times(1)).getAll();
         assertEquals(newsList, result);
     }
 
@@ -64,19 +65,19 @@ public class NewsServiceImplTest {
     public void get() {
         when(dao.get(TEST_ID)).thenReturn(news);
         News result = service.get(TEST_ID);
-        verify(dao).get(TEST_ID);
+        verify(dao, times(1)).get(TEST_ID);
         assertEquals(news, result);
     }
 
     @Test
     public void update() {
         service.update(TEST_ID, news);
-        verify(dao).update(TEST_ID, news);
+        verify(dao, times(1)).update(TEST_ID, news);
     }
 
     @Test
     public void delete() {
         service.delete(TEST_ID);
-        verify(dao).delete(TEST_ID);
+        verify(dao, times(1)).delete(TEST_ID);
     }
 }
